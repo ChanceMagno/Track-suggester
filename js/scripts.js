655fbdba1730525eee90cdfe3acb8b124646055f
chance <chancemagno@gmail.com>
2017-02-17 14:10:10 -0800

    add form to get name, update text and add hre in classes php, java, and swift

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $("form").submit(function() {
+  $("form#questions").submit(function() {
+    var getName = $("input#name").val();
     var interest = parseInt($("select#question1").val());
     var appealing = parseInt($("select#question2").val());
     var demand = parseInt($("select#question3").val());
@@ -7,28 +8,30 @@ $(document).ready(function(){
     var color = parseInt($("select#question5").val());
     var totalScore = interest + appealing + demand + popularity + color;
 
+
+
     if (interest === 0 || appealing === 0 || demand === 0 || popularity ===0 || color ===0) {
       $(".alert-danger").show();
 
     } else {
       if(totalScore <= 5){
+        $(".userName").text(getName).show();
         $(".java").show();
         $(".swift, .php").hide();
 
-
       } else if ((totalScore > 5) && (totalScore < 9)) {
+          $(".userName").text(getName).show();
           $(".swift").show();
           $(".php, .java").hide();
 
-
       }  else {
+          $(".userName").text(getName).show();
           $(".php").show();
           $(".swift, .java").hide();
 
       }
-      }
-
+    }
 
     event.preventDefault();
-  });
+    });
 });
